60297f9d3a4d46103c07a894c9a799cb48b90d92
dedmodo <36816219+dedmodo@users.noreply.github.com>
2020-09-18 00:15:13 +0200

    Add files via upload

---
+++ Nicehash.bat
@@ -0,0 +1,2 @@
+EthDcrMiner64.exe -epool daggerhashimoto.eu.nicehash.com:3353 -ewal 3JGhkL3n5jUzbessKwnMMS6q1Vmj8Hgqg9 -epsw x -dbg -1 -retrydelay 1 -ftime 55   -r 0 -esm 3 -erate 1 -allcoins 1 -allpools 1
+pause
